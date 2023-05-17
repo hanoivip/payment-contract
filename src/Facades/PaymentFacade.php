cef7f87a77f3bffee5343865ba4a3cf32e211468
unknown <game.oh.vn@gmail.com>
2023-05-17 09:00:51 +0700

    f

--- src/Facades/PaymentFacade.php
+++ src/Facades/PaymentFacade.php
@@ -4,6 +4,9 @@ namespace Hanoivip\PaymentContract\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static Hanoivip\PaymentMethodContract\IPaymentResult query($transId, $force = false)
+ */ 
 class PaymentFacade extends Facade
 {
     protected static function getFacadeAccessor()
